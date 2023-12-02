d9b3644fc6eb101fedb18c4a7156dab97c6e8904
Jose Lacueva <xkraum@gmail.com>
2023-12-02 18:55:43 +0100

    Created base mod

---
+++ src/main/java/com/shulkervacuum/ShulkerVacuum.java
@@ -0,0 +1,14 @@
+package com.shulkervacuum;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ShulkerVacuum implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("shulkervacuum");
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Hello Fabric world!");
+    }
+}
